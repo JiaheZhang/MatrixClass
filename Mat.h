85c3108413a464113032687a07378cf4d89b3b6e
myEvanZhang <zhangjiahe22@163.com>
2016-11-03 11:43:25 +0800

    初步实现矩阵类

---
+++ Mat.h
@@ -0,0 +1,38 @@
+#ifndef MAT_H_
+#define MAT_H_
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Mat
+{
+	public:
+		Mat(); //无参构造函数 
+		Mat(int rows,int cols);
+		Mat(Mat& newMat);
+		void display(void) const;
+		int& at(int rows,int cols);
+		int getRows(void) const;
+		int getCols(void) const;
+		int getSize(void) const;
+		friend ostream& operator << (ostream &newOstream,Mat &newMat);
+		bool operator == (Mat & newMat)const;
+		Mat& operator = (Mat &newMat);
+		Mat& operator + (Mat & newMat);
+		Mat& operator - (Mat & newMat);
+		Mat operator -= (Mat & newMat);
+		Mat operator += (Mat & newMat);
+	
+	private:
+		private:
+		int rows;
+		int cols;
+		int size;
+		vector<vector<int> > vec;
+
+};
+
+
+#endif
+
